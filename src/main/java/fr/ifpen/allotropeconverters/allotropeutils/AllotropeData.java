a1aa163e6262dae11cdfb994915d2288c5241027
viscontm <maxime.visconte@ifpen.fr>
2023-03-17 11:48:20 +0100

    prepare for maven central

--- src/main/java/fr/ifpen/allotropeconverters/allotropeutils/AllotropeData.java
+++ src/main/java/fr/ifpen/allotropeconverters/allotropeutils/AllotropeData.java
@@ -2,4 +2,10 @@ package fr.ifpen.allotropeconverters.allotropeutils;
 
 import java.util.List;
 
+
+/**
+ * Represents an Allotrope data cube
+ * @param dimensions the X axis values
+ * @param measures the Y axis values
+ */
 public record AllotropeData(List<List<Double>> dimensions, List<List<Double>> measures){}
